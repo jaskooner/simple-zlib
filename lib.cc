96dba75973d4f7f5a79d0c5a31eeb8c6bb0e4509
scooter <jaskooner.singh@gmail.com>
2019-03-20 16:23:49 +0000

    extracted functionality from main to library

---
+++ lib.cc
@@ -0,0 +1,85 @@
+#include "lib.hpp"
+
+
+int def (FILE *source, FILE *dest, int level)
+{
+  int ret, flush;
+  unsigned have;
+  z_stream strm;
+  unsigned char in[CHUNK];
+  unsigned char out[CHUNK];
+
+  /* allocate defalte state */
+  strm.zalloc = Z_NULL;
+  strm.zfree = Z_NULL;
+  strm.opaque = Z_NULL;
+  ret = deflateInit(&strm, level);
+  if (ret != Z_OK)
+    return ret;
+
+  /* compress until end of file */
+  do
+  {
+    strm.avail_in = fread(in, 1, CHUNK, source);
+    if (ferror(source))
+    {
+      (void)deflateEnd(&strm);
+      return Z_ERRNO;
+    }
+    flush = feof(source) ? Z_FINISH : Z_NO_FLUSH;
+    strm.next_in = in;
+
+    /* run deflate() on input until output buffer not full, 
+    finish compression if all of source has been read in */
+
+    do
+    {
+      strm.avail_out = CHUNK;
+      strm.next_out = out;
+
+      ret = deflate(&strm, flush); /* no bad return value */
+      assert(ret != Z_STREAM_ERROR);
+
+      have = CHUNK - strm.avail_out;
+      if (fwrite(out, 1, have, dest) != have || ferror(dest))
+      {
+        (void)deflateEnd(&strm);
+        return Z_ERRNO;
+      }
+
+    } while (strm.avail_out == 0);
+    assert(strm.avail_in == 0); /* all input will be used */
+
+    /* done when last data in file processed */
+  } while (flush != Z_FINISH);
+  assert(ret == Z_STREAM_END); /* stream will be complate */
+
+  /*clean up an dreturn */
+  (void)deflateEnd(&strm);
+  return Z_OK;
+}
+
+void zerr(int ret)
+{
+  fputs("zpipe: ", stderr);
+  switch (ret)
+  {
+  case Z_ERRNO:
+    if (ferror(stdin))
+      fputs("error reading stdin\n", stderr);
+    if (ferror(stdout))
+      fputs("error writing stdout\n", stderr);
+    break;
+  case Z_STREAM_ERROR:
+    fputs("invalid compression level\n", stderr);
+    break;
+  case Z_DATA_ERROR:
+    fputs("invalid or incomplete deflate data\n", stderr);
+    break;
+  case Z_MEM_ERROR:
+    fputs("out of memory\n", stderr);
+    break;
+  case Z_VERSION_ERROR:
+    fputs("zlib version mismatch!\n", stderr);
+  }
+}
---
+++ lib.hpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+#include "zlib-library/release/include/zlib.h"\\
+
+/* CHUNK is simply the buffer size for feeding data to and pulling data from the zlib routines. Larger buffer sizes would be more efficient, especially for inflate(). If the memory is available, buffers sizes on the order of 128K or 256K bytes should be used.*/
+
+#define CHUNK 16384
+
+/* Compress from file source to file dest until EOF on source.
+   def() returns Z_OK on success, Z_MEM_ERROR if memory could not be
+   allocated for processing, Z_STREAM_ERROR if an invalid compression
+   level is supplied, Z_VERSION_ERROR if the version of zlib.h and the
+   version of the library linked do not match, or Z_ERRNO if there is
+   an error reading or writing the files. */
+
+int def (FILE *source, FILE *dest, int Level);
+
+/* report a zlib or i/o error */
+void zerr(int ret);
+
